9e99be9d619f2010e8d5309938db873605a5c55d
Gregore Silva <gregore.silva@maqina.com.br>
2022-11-04 23:21:59 -0300

    final version test

--- public/js/index.js
+++ public/js/index.js
@@ -30,7 +30,7 @@ buttonAdd.addEventListener("click", close)
 var element = document.querySelector('.card-db')
 
 const cardsAdd = () =>{
-    fetch("http://localhost:3000/cards")
+    fetch("https://schedule-4qhn.vercel.app/cards")
     .then(res => res.json())
     .then(content => {
         callCard(content)
@@ -42,39 +42,46 @@ const callCard = (content) => {
     for(var i = 0; i < content.length; i++){
         var createDiv = document.createElement('div')
         createDiv.classList.add('card-db')
-
+        
         const divPai = document.querySelector('.content');
-
+        
         if(content[i].date == 0){
-            console.log(' teste')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[0].appendChild(createDiv)
         }
         else if(content[i].date == 1){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[1].appendChild(createDiv)
         }
         else if(content[i].date == 2){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[2].appendChild(createDiv)
         }
         else if(content[i].date == 3){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[3].appendChild(createDiv)
         }
         else if(content[i].date == 4){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[4].appendChild(createDiv)
         }
         else if(content[i].date == 5){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[5].appendChild(createDiv)
         }
         else if(content[i].date == 6){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[6].appendChild(createDiv)
         }
         else if(content[i].date == 7){
-            console.log(' teste2')
+            createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
+            divPai.children[7].appendChild(createDiv)
         }
         else{
             console('nada')
         }
-
-        createDiv.innerHTML = `Atendimento: ${content[i].username}<br>Cliente: ${content[i].client}<br>Início: ${content[i].horaEntrada}<br>Saída: ${content[i].horaSaida} `
-        element.appendChild(createDiv)
+        
+        
     }
 }
 
